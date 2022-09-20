a1c7ed81959a6980815b73de636e875b3dbda445
Denys Stovbun <denis.stovbun@lanars.com>
2022-09-20 12:19:20 +0300

    add project note

--- Application/main.c
+++ Application/main.c
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ Processor:                     STM32
+ Compiler:                      gcc
+ Version:                       0.0.1
+ Comment:                       Test DMA copy from ADC to memory
+                                ADC1 from PA0 (Analog) (blue button pin)
+                                DMA2 Stream 0 - Channel 0 PeripheralToMemory
+
+*******************************************************************************/
 #include "project_config.h"
 
 //======================================== MAIN ===========================================
